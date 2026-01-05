148a5064c10a4aee0787e3d6fc47207f2aa0be8f
Fadi <Fadi@1.1.13.220>
2026-01-05 12:57:05 +0300

    Add ElectricCar class

---
+++ src/carrental/ElectricCar.java
@@ -0,0 +1,28 @@
+package carrental;
+
+public class ElectricCar extends Car {
+    private double batteryCapacity; // kWh
+    private double chargingTime; // hours
+
+    public ElectricCar(String plateNumber, String brand, String model, int year, 
+                      double dailyPrice, double batteryCapacity, double chargingTime) {
+        super(plateNumber, brand, model, year, dailyPrice);
+        this.batteryCapacity = batteryCapacity;
+        this.chargingTime = chargingTime;
+    }
+
+    @Override
+    public double calculateRentalFee(int days) {
+        double baseFee = getDailyPrice() * days;
+        double discount = baseFee * 0.10; // 10% discount for electric cars
+        return baseFee - discount;
+    }
+
+    public double getBatteryCapacity() { return batteryCapacity; }
+    public double getChargingTime() { return chargingTime; }
+    
+    @Override
+    public String toString() {
+        return getCarInfo() + " [Electric - " + batteryCapacity + "kWh]";
+    }
+}
