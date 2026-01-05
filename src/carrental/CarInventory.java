c040d46b7de78728cfd8c8f0b9647daf54d08d69
Fadi <Fadi@1.1.13.220>
2026-01-05 13:43:10 +0300

    Minor bug fixes and cleanup

--- src/carrental/CarInventory.java
+++ src/carrental/CarInventory.java
@@ -7,7 +7,7 @@ import java.util.stream.Collectors;
 public class CarInventory {
     private ArrayList<Car> cars;
 
-    public CarInventory() {
+    public CarInventory() { 
         this.cars = new ArrayList<>();
     }
 
