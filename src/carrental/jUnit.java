3f3187bec8e9546a6bd0180b64ab55f6f28ef884
Fadi <Fadi@1.1.13.220>
2026-01-05 12:57:32 +0300

    Add jUnit class

---
+++ src/carrental/jUnit.java
@@ -0,0 +1,14 @@
+package carrental;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class jUnit {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
